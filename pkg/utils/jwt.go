12d019efcbf9989f64c50a345d385de0dddf92b8
tnnz20 <kaitora200@gmail.com>
2024-03-28 08:25:17 +0800

    feat: Add ClaimID function to retrieve user ID from JWT token

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -1,5 +1,14 @@
 package utils
 
-func ClaimToken() {
-	// Your code here
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/tnnz20/final-hacktiv8-fga/internal/adapters/handler/http/middleware"
+)
+
+func ClaimID(c echo.Context) int {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*middleware.JWTCustomClaims)
+	claimId := claims.ID
+	return claimId
 }
