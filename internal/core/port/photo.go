b911ef4b733379f2c0517cc9d2aacc9d476e76c6
tnnz20 <kaitora200@gmail.com>
2024-03-28 17:52:51 +0800

    feat: Update PhotoRepository and PhotoService interfaces

--- internal/core/port/photo.go
+++ internal/core/port/photo.go
@@ -10,14 +10,15 @@ type PhotoRepository interface {
 	Create(ctx context.Context, photo *domain.Photo) (*domain.Photo, error)
 	Update(ctx context.Context, photo *domain.Photo) (*domain.Photo, error)
 	Delete(ctx context.Context, id int) error
-	FindByID(ctx context.Context, id int) (*domain.Photo, error)
-	FindAll(ctx context.Context) ([]domain.Photo, error)
+	FindByID(ctx context.Context, id int) (*domain.GetPhoto, error)
+	FindAll(ctx context.Context) (*[]domain.GetPhoto, error)
+	FindByUserID(ctx context.Context, userId int) (*domain.Photo, error)
 }
 
 type PhotoService interface {
-	Create(ctx context.Context, req *domain.CreatePhotoRequest) (*domain.CreatePhotoResponse, error)
-	Update(ctx context.Context, req *domain.UpdatePhotoRequest) (*domain.UpdatePhotoResponse, error)
-	Delete(ctx context.Context, id int) error
-	FindByID(ctx context.Context, id int) (*domain.GetPhoto, error)
-	FindAll(ctx context.Context) ([]domain.GetPhoto, error)
+	Create(ctx context.Context, req *domain.CreatePhotoRequest, userId int) (*domain.CreatePhotoResponse, error)
+	Update(ctx context.Context, req *domain.UpdatePhotoRequest, photoId, userId int) (*domain.UpdatePhotoResponse, error)
+	Delete(ctx context.Context, photoId, userId int) error
+	GetByID(ctx context.Context, id int) (*domain.GetPhoto, error)
+	GetAll(ctx context.Context) (*[]domain.GetPhoto, error)
 }
