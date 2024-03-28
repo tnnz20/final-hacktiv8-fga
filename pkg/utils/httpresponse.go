9b53fdcce36ab5e9cb5dbfca901a6ec1f0ecf8a2
tnnz20 <kaitora200@gmail.com>
2024-03-28 23:21:08 +0800

    Update ErrorResponse function signature to accept string message

--- pkg/utils/httpresponse.go
+++ pkg/utils/httpresponse.go
@@ -2,7 +2,7 @@ package utils
 
 import "github.com/labstack/echo/v4"
 
-func ErrorResponse(c echo.Context, statusCode int, message interface{}) error {
+func ErrorResponse(c echo.Context, statusCode int, message string) error {
 	return c.JSON(statusCode, echo.Map{
 		"status":  "error",
 		"message": message,
