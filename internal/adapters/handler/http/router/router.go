e136c3cbd2a8fa809e12a14785949578e242dbe0
tnnz20 <kaitora200@gmail.com>
2024-03-28 17:55:15 +0800

    Refactor user, photo, and comment routers

--- internal/adapters/handler/http/router/router.go
+++ internal/adapters/handler/http/router/router.go
@@ -4,10 +4,9 @@ import (
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/tnnz20/final-hacktiv8-fga/internal/adapters/handler/http"
-	"github.com/tnnz20/final-hacktiv8-fga/internal/adapters/handler/http/middleware"
 )
 
-func NewUserRouter(e *echo.Echo, key *string, h *http.UserHandler) {
+func NewUserRouter(e *echo.Echo, h *http.UserHandler, config echojwt.Config) {
 
 	// Public Route
 	user := e.Group("/users")
@@ -17,9 +16,37 @@ func NewUserRouter(e *echo.Echo, key *string, h *http.UserHandler) {
 	// Protected Route
 	protected := e.Group("/users")
 
-	config := middleware.JWTconfig(key)
-
 	protected.Use(echojwt.WithConfig(config))
 	protected.PUT("/update", h.Update)
 	protected.DELETE("", h.Delete)
+}
+
+func NewPhotoRouter(e *echo.Echo, h *http.PhotoHandler, config echojwt.Config) {
+
+	// Public Route
+	photo := e.Group("/photos")
+	photo.GET("", h.GetAll)
+	photo.GET("/:photoId", h.GetByID)
+
+	// Protected Route
+	protected := e.Group("/photos")
+	protected.Use(echojwt.WithConfig(config))
+	protected.POST("", h.Create)
+	protected.PUT("/:photoId", h.Update)
+	protected.DELETE("/:photoId", h.Delete)
+}
+
+func NewCommentRouter(e *echo.Echo, h *http.CommentHandler, config echojwt.Config) {
+
+	// Public Route
+	// comment := e.Group("/comments")
+	// comment.GET("", h.GetAll)
+	// comment.GET("/:id", h.GetByID)
+
+	// Protected Route
+	protected := e.Group("/comments")
+	protected.Use(echojwt.WithConfig(config))
+	protected.POST("", h.Create)
+	// protected.PUT("/:id", h.Update)
+	// protected.DELETE("/:id", h.Delete)
 }
