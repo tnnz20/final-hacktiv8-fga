62222e91a2332844247cc411708c10e9225e35d6
tnnz20 <kaitora200@gmail.com>
2024-03-28 17:54:40 +0800

    Add error messages for user authentication and validation

--- internal/core/domain/error.go
+++ internal/core/domain/error.go
@@ -3,8 +3,14 @@ package domain
 import "errors"
 
 var (
+	ErrUnauthorized = errors.New("you are not authorized to update")
+	ErrInvalidId    = errors.New("invalid id must be a number")
+
 	ErrUserNotFound  = errors.New("user not found")
+	ErrEmailExist    = errors.New("email already exists")
+	ErrUsernameExist = errors.New("username already exists")
+	ErrWrongPassword = errors.New("wrong password")
+
 	ErrPhotoNotFound = errors.New("photo not found")
 	ErrPhotosEmpty   = errors.New("photos still empty")
-	ErrUnauthorized  = errors.New("you are not authorized to update")
 )
